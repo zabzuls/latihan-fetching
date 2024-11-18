597f99c1d9b39581349bdc2c4d9812d3225ff09b
Zabzul Supratman <zabzuls@gmail.com>
2024-11-18 08:37:52 +0700

    [FEAT] remove link in navbar

--- src/Components/NavbarPage/NavbarPage.tsx
+++ src/Components/NavbarPage/NavbarPage.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Zabzul Supratman</span>
-        </a>
-    
+        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          Zabzul Supratman
+        </span>
+
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
