d9418f445803fcce58a9b4b401e2d171fe10ad78
Zabzul Supratman <zabzuls@gmail.com>
2024-11-18 13:55:22 +0700

    [FEAT] Add Ui Components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,27 @@
-import daisyui from "daisyui";
+
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [daisyui],
+  
   daisyui: {
     themes: ["light", "dark", "cupcake"],
   },
